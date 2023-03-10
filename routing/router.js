dd2258d9f4f3d4b37cadc818cf553b268a87d361
Jackson Mwaniki <66781793+jakkkc@users.noreply.github.com>
2023-03-10 11:55:17 +0300

    Add files via upload

---
+++ routing/router.js
@@ -0,0 +1,24 @@
+const route = (event) => {
+  event = event || window.event;
+  event.preventDefault();
+  window.history.pushState({}, "", event.target.href);
+  handleLocation();
+};
+
+const routes = {
+  404: "/pages/404.html",
+  "/": "/pages/index.html",
+  "/about": "/pages/about.html",
+  "/lorem": "/pages/lorem.html",
+};
+
+const handleLocation = async () => {
+  const path = window.location.pathname;
+  const route = route[path] || routes[404];
+  const html = await fetch(route).then((data) => data.text());
+  document.getElementById("main-page").innerHTML = html;
+};
+
+window.onpopstate = handleLocation;
+window.route = route;
+handleLocation();
